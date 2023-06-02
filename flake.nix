e54a24a40ea59a5959f4658dd108e65542b99144
dannywood10 <info.dannywood@gmail.com>
2023-06-02 09:00:10 +0100

    removed nix-colors

--- flake.nix
+++ flake.nix
@@ -8,7 +8,6 @@
     home-manager.url = "github:nix-community/home-manager/release-22.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    nix-colors.url = "github:misterio77/nix-colors";
     hardware.url = "github:nixos/nixos-hardware";
   };
 
